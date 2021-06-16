66695281851ebdc082f71afb116580a6e5cdfdae
Elmar Abdullayev <elmarsha@code.edu.az>
2021-06-17 03:26:21 +0400

    updated  reactapp

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import About from './components/About';
 import Banner from './components/Banner'
 import Nav from './components/Nav';
+import Portfolio from './components/Portfolio';
 import Services from './components/Services';
 import Skills from './components/Skills';
 
@@ -19,6 +20,7 @@ function App() {
       <Services/>
       <About/>
       <Skills/>
+      <Portfolio/>
     </div>
   );
 }
